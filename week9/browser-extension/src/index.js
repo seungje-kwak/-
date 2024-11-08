60aa3649539a471af22e0fea21be6d219070b38a
a01072512078 <a01072512078@gmail.com>
2024-11-08 19:20:38 +0900

    carbon process

--- week9/browser-extension/src/index.js
+++ week9/browser-extension/src/index.js
@@ -1,53 +1,120 @@
+import axios from '../node_modules/axios';
+
 // form fields
 const form = document.querySelector('.form-data');
-const region = document.querySelector('.region-name');
+const regions = document.querySelectorAll('.region-name');
 const apiKey = document.querySelector('.api-key');
+
 // results
 const errors = document.querySelector('.errors');
 const loading = document.querySelector('.loading');
-const results = document.querySelector('.result-container');
-const usage = document.querySelector('.carbon-usage');
-const fossilfuel = document.querySelector('.fossil-fuel');
-const myregion = document.querySelector('.my-region');
+const results = document.querySelectorAll('.result-container');
+const usages = document.querySelectorAll('.carbon-usage');
+const fossilfuels = document.querySelectorAll('.fossil-fuel');
+const myregions = document.querySelectorAll('.my-region');
 const clearBtn = document.querySelector('.clear-btn');
+
+const calculateColor = async (value) => {
+    let co2Scale = [0, 150, 600, 750, 800];
+    let colors = ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'];
+    let closestNum = co2Scale.sort((a, b) => {
+        return Math.abs(a - value) - Math.abs(b - value);
+    })[0];
+    console.log(value + ' is closest to ' + closestNum);
+    let num = (element) => element > closestNum;
+    let scaleIndex = co2Scale.findIndex(num);
+
+    let closestColor = colors[scaleIndex];
+    console.log(scaleIndex, closestColor);
+    chrome.runtime.sendMessage({ action: 'updateIcon', value: { color: closestColor } });
+    };
+
+const displayCarbonUsage = async (apiKey, region, index) => {
+    try {
+        await axios
+            .get('https://api.co2signal.com/v1/latest', {
+                params: {
+                    countryCode: region,
+                },
+                headers: {
+                    //please get your own token from CO2Signal https://www.co2signal.com/
+                    'auth-token': apiKey,
+                },
+            })
+            .then((response) => {
+                let CO2 = Math.floor(response.data.data.carbonIntensity);
+            
+                calculateColor(CO2);
+            
+                loading.style.display = 'none';
+                form.style.display = 'none';
+                myregions[index].textContent = region;
+                usages[index].textContent = Math.round(response.data.data.carbonIntensity) + ' grams (grams CO2 emitted per kilowatt hour)';
+                fossilfuels[index].textContent = response.data.data.fossilFuelPercentage.toFixed(2) + '% (percentage of fossil fuels used to generate electricity)';
+                results[index].style.display = 'block';
+            });
+    } catch (error) {
+        console.log(error);
+        loading.style.display = 'none';
+        errors.textContent = `Sorry, we have no data for the region ${results[index]} you have requested.`;
+    } 
+};
+
+function setUpUser(apiKey, regionName1,regionName2, regionName3) {
+    localStorage.setItem('apiKey', apiKey);
+    localStorage.setItem('regionName1', regionName1);
+    localStorage.setItem('regionName2', regionName2);
+    localStorage.setItem('regionName3', regionName3);
+    loading.style.display = 'block';
+    errors.textContent = '';
+    clearBtn.style.display = 'block';
+    displayCarbonUsage(apiKey, regionName1, 0);
+    displayCarbonUsage(apiKey, regionName2, 1);
+    displayCarbonUsage(apiKey, regionName3, 2);
+   };
+   
+function handleSubmit(e) {
+    e.preventDefault();
+    const regionNames = Array.from(regions).map(region => region.value);
+    setUpUser(apiKey.value, regionNames[0], regionNames[1], regionNames[2]);
+};
 
 function init() {
     const storedApiKey = localStorage.getItem('apiKey');
-    const storedRegion = localStorage.getItem('regionName');
+    const storedRegion1 = localStorage.getItem('regionName1');
+    const storedRegion2 = localStorage.getItem('regionName2');
+    const storedRegion3 = localStorage.getItem('regionName3');
     //set icon to be generic green
     //todo
-    if (storedApiKey === null || storedRegion === null) {
+    chrome.runtime.sendMessage({
+        action: 'updateIcon',
+            value: {
+               color: 'green',
+        },
+    });
+    if (storedApiKey === null || storedRegion1 === null || storedRegion2 === null || storedRegion3 === null) {
     form.style.display = 'block';
-    results.style.display = 'none';
-    loading.style.display = 'none';
+    results.forEach((result) => {
+        result.style.display = 'none';
+    });
+    loading.style.display = 'none'; 
     clearBtn.style.display = 'none';
     errors.textContent = '';
     } else {
-    displayCarbonUsage(storedApiKey, storedRegion);
-    results.style.display = 'block';
+    displayCarbonUsage(storedApiKey, storedRegion1, 0);
+    displayCarbonUsage(storedApiKey, storedRegion2, 1);
+    displayCarbonUsage(storedApiKey, storedRegion3, 2);
     form.style.display = 'none';
     clearBtn.style.display = 'block';
     }
 };
-   
-function handleSubmit(e) {
-    e.preventDefault();
-    setUpUser(apiKey.value, region.value);
-}
-
-function setUpUser(apiKey, regionName) {
-    localStorage.setItem('apiKey', apiKey);
-    localStorage.setItem('regionName', regionName);
-    loading.style.display = 'block';
-    errors.textContent = '';
-    clearBtn.style.display = 'block';
-    displayCarbonUsage(apiKey, regionName);
-   }
-   
 
 function reset(e) {
     e.preventDefault();
-    localStorage.removeItem('regionName');
+    localStorage.removeItem('apiKey');
+    localStorage.removeItem('regionName1');
+    localStorage.removeItem('regionName2');
+    localStorage.removeItem('regionName3');
     init();
 }
 
